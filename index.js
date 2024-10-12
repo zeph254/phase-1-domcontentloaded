ba93149812d798e8544883fe004e33ba961d93b2
zephania owuor <ularezephaniah@gmail.com>
2024-10-12 19:10:04 +0300

    domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+  document.addEventListener('DOMContentLoaded', function () {
+    // Target the paragraph element with id="text"
+    const paragraph = document.getElementById('text');
+  
+    // Replace its content with "This is really cool!"
+    paragraph.textContent = 'This is really cool!';
+  });
+  
